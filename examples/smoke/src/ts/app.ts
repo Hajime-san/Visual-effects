de0e973363ceb4706891c689e2326b4651c2c0f6
Hajime-san <utd.c.r.d.s.a@gmail.com>
2020-06-20 01:15:57 +0900

    wip

--- examples/smoke/src/ts/app.ts
+++ examples/smoke/src/ts/app.ts
@@ -6,10 +6,15 @@ let camera: THREE.PerspectiveCamera,
   renderer: THREE.WebGLRenderer,
   geometry: THREE.BufferGeometry,
   material: THREE.PointsMaterial,
+  material2: THREE.PointsMaterial,
+  mat: THREE.ShaderMaterial,
   textureLoader: THREE.TextureLoader,
+  uniforms: any,
   // mesh: THREE.Mesh,
   uvAnimation: FlipBook,
-  time: THREE.Clock;
+  uvAnimation2: FlipBook,
+  time: number,
+  delta: THREE.Clock;
 
 const frac = (float: number) => float % 1;
 
@@ -83,7 +88,9 @@ const init = () => {
 
   scene = new THREE.Scene();
 
-  time = new THREE.Clock();
+  time = 0;
+
+  delta = new THREE.Clock();
 
 
   geometry = new THREE.BufferGeometry();
@@ -92,6 +99,7 @@ const init = () => {
 
   const texture = textureLoader.load("./assets/T_Smoke_SubUV.png");
   const texture2 = textureLoader.load("./assets/T_Smoke_Tiled_D.jpg");
+
 
   const meshFloor = new THREE.Mesh(
     new THREE.BoxGeometry(200, 0.1, 200),
@@ -110,12 +118,14 @@ const init = () => {
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(1, 100, 10);
   scene.add(directionalLight);
-  const pointLightHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
-  scene.add(pointLightHelper);
+  // const pointLightHelper = new THREE.DirectionalLightHelper(directionalLight, 10);
+  // scene.add(pointLightHelper);
 
 
 
   uvAnimation = new FlipBook( texture, 8, 8, 64 );
+
+  uvAnimation2 = new FlipBook( texture2, 8, 8, 64 );
 
   let vertices = [];
 
@@ -128,23 +138,59 @@ const init = () => {
 
   geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
 
+  const geo = new THREE.PlaneGeometry(40, 40);
+
   material = new THREE.PointsMaterial({
     size: 50,
     map: texture,
     blending: THREE.AdditiveBlending,
     depthTest: true,
     transparent: true,
-    alphaMap: texture,
-    blendDstAlpha: 100,
     sizeAttenuation: true
+  });
+
+  material2 = new THREE.PointsMaterial({
+    size: 50,
+    map: texture2,
+    blending: THREE.MultiplyBlending,
+    depthTest: true,
+    transparent: true,
+    sizeAttenuation: true,
+  });
+
+  uniforms = {
+    uTex: { value: texture },
+    uTex2: { value: texture2 },
+    uFixAspect: {
+      value: 1 / 1
+    },
+    time: {
+      value: 0.0
+    }
+  };
+
+   mat = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: vertexSource(),
+    fragmentShader: fragmentSource(),
+    depthTest: true,
+    transparent: true
   });
 
   const particles = new THREE.Points(geometry, material);
 
+  particles.translateX(-50);
   particles.translateY(20);
 
   scene.add(particles);
 
+
+  const mesh = new THREE.Mesh(geo, mat);
+
+  mesh.translateY(20);
+  mesh.translateZ(0);
+
+  scene.add(mesh);
 
   window.addEventListener("resize", onWindowResize, false);
 };
@@ -154,10 +200,11 @@ let update = 0;
 const animate = () => {
   requestAnimationFrame(animate);
 
-  const delta = time.getDelta();
+  let frame = delta.getDelta();
 
-  uvAnimation.update(1000 * delta);
+  uvAnimation.update(1000 * frame);
 
+  uvAnimation2.update(1000 * frame);
 
   // for (let i = 0; i < 10; i++) {
   //   geometry.attributes.position.setXYZ(i, Math.random() * 100 - 50, i + 10 , Math.random() * 100 - 50)
@@ -175,6 +222,12 @@ const animate = () => {
 
   //geometry.attributes.position.needsUpdate = true;
 
+  time += frame;
+
+  mat.uniformsNeedUpdate = true;
+
+  uniforms.time.value = time;
+
   renderer.render(scene, camera);
 };
 
@@ -188,3 +241,51 @@ document.addEventListener("DOMContentLoaded", () => {
   init();
   animate();
 });
+
+
+function vertexSource() {
+  return `
+    varying vec2 vUv;
+
+    uniform float uFixAspect;
+    uniform sampler2D uTex;
+    uniform sampler2D uTex2;
+
+    void main() {
+      vUv = uv - .5;
+      vUv.y *= uFixAspect;
+      vUv += .5;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position , 1.0);
+    }
+  `
+}
+
+
+function fragmentSource() {
+  return `
+    #define COLUMN 8.0
+    #define ROW 8.0
+
+    varying vec2 vUv;
+
+    uniform sampler2D uTex;
+    uniform sampler2D uTex2;
+    uniform float time;
+
+    void main() {
+
+      float frame = 16.0;
+
+      float offsetX = (vUv.x + floor(time * frame)) / COLUMN;
+      float offsetY = (vUv.y + floor(time * frame)) / COLUMN;
+
+      vec4 color = texture2D( uTex, vec2(offsetX, offsetY) ).rgba;
+      vec3 color2 = texture2D( uTex2, vec2(offsetX, offsetY) ).rgb;
+      vec4 finalColor = vec4(color.rgb + color2, color.a);
+
+      gl_FragColor = finalColor;
+    }
+
+  `
+}
