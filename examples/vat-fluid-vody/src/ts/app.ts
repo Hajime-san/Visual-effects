f622490c23252f7d25ad1f1243dc522e9c75b6dd
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2020-08-20 16:25:30 +0900

    relase vat fluid (#7)
    * init
    
    * add normal
    
    * add sample hlsl code
    
    * almost work
    
    * build

---
+++ examples/vat-fluid-vody/src/ts/app.ts
@@ -0,0 +1,186 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as dat from 'dat.gui';
+import { loadShaders, ShaderData, onWindowResize, loadGLTF, loadVATexrTexture, loadTexture } from '../../../modules/Util';
+
+import data from '../../dist/assets/model/RubberToy_data.json';
+
+let camera: THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
+let mesh: THREE.Mesh<THREE.BufferGeometry, THREE.ShaderMaterial>;
+let uniforms: { [uniform: string]: THREE.IUniform };
+let time: number;
+let delta: THREE.Clock;
+let currentFrame = 0;
+let shaderData: ShaderData;
+const VATdata = data[0];
+
+class GuiUniforms {
+    totalFrame: number;
+
+    dynamicBaseColor: Array<number>;
+
+    constructor(totalFrame: number) {
+        this.totalFrame = totalFrame;
+        this.dynamicBaseColor = [20, 244, 0];
+    }
+}
+
+const init = async () => {
+    // dat GUI
+    const parameters = new GuiUniforms(VATdata.numOfFrames);
+    const gui = new dat.GUI();
+    gui.add(parameters, 'totalFrame', VATdata.numOfFrames * 0.5, VATdata.numOfFrames * 2).onChange(() => {
+        uniforms.totalFrame.value = parameters.totalFrame;
+        currentFrame = 0;
+    });
+    gui.addColor(parameters, 'dynamicBaseColor').onChange(() => {
+        uniforms.dynamicBaseColor.value = parameters.dynamicBaseColor;
+    });
+
+    // intial settings
+    const container = document.getElementById('canvas');
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.set(0, 30, 70);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    time = 0;
+
+    delta = new THREE.Clock();
+
+    window.addEventListener('resize', () => onWindowResize(camera, renderer), false);
+
+    // floor
+    const meshFloor = new THREE.Mesh(
+        new THREE.BoxGeometry(200, 0.1, 200),
+        new THREE.MeshStandardMaterial({
+            color: 0x808080,
+            roughness: 0,
+            metalness: 0.5,
+        })
+    );
+    scene.add(meshFloor);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(1, 1, -10);
+    scene.add(directionalLight);
+
+    const light = new THREE.AmbientLight(0xffffff, 1.0);
+    scene.add(light);
+
+    const positionMap = await loadVATexrTexture('./assets/images/RubberToy_position.exr');
+
+    // 8-bit png converted from exr
+    const normalMap = await loadTexture('./assets/images/RubberToy_normal.png');
+    normalMap.encoding = THREE.LinearEncoding;
+    normalMap.minFilter = THREE.LinearFilter;
+    normalMap.magFilter = THREE.NearestFilter;
+
+    // set shader
+    shaderData = await loadShaders([
+        { key: 'vertex', path: './assets/shaders/shader.vert' },
+        { key: 'fragment', path: './assets/shaders/shader.frag' },
+    ]);
+
+    const model = await loadGLTF('./assets/model/RubberToy.glb');
+
+    mesh = model.scenes[0].children[0] as THREE.Mesh<THREE.BufferGeometry, THREE.ShaderMaterial>;
+
+    let colorMap: THREE.Texture;
+
+    model.parser.associations.forEach((value, key: THREE.Texture) => {
+        if (value.type === 'textures' && value.index === 0) {
+            colorMap = key;
+        }
+    });
+
+    const indicesLength = positionMap.image.width;
+
+    uniforms = {
+        positionMap: {
+            value: positionMap,
+        },
+        normalMap: {
+            value: normalMap,
+        },
+        colorMap: {
+            value: colorMap,
+        },
+        time: {
+            value: 0.0,
+        },
+        // set bounding box for correct scale
+        boudingBoxMax: {
+            value: VATdata.posMax,
+        },
+        // set bounding box for correct scale
+        boundingBoxMin: {
+            value: VATdata.posMin,
+        },
+        indicesLength: {
+            value: indicesLength,
+        },
+        // total animation frame
+        totalFrame: {
+            value: VATdata.numOfFrames,
+        },
+        currentFrame: {
+            value: 0,
+        },
+        dynamicBaseColor: {
+            value: parameters.dynamicBaseColor,
+        },
+
+        // lights
+        ...THREE.UniformsLib.lights,
+    };
+
+    mesh.material = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: shaderData.vertex,
+        fragmentShader: shaderData.fragment,
+        depthTest: true,
+        lights: true,
+        extensions: {
+            derivatives: true,
+        },
+    });
+
+    mesh.position.set(0, 0.5, 0);
+    mesh.scale.set(10, 10, 10);
+    scene.add(mesh);
+};
+
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    const frame = delta.getDelta();
+
+    time += frame;
+
+    mesh.material.uniforms.time.value = time;
+
+    currentFrame += 1;
+
+    mesh.material.uniforms.currentFrame.value = currentFrame;
+
+    if (currentFrame === Math.floor(mesh.material.uniforms.totalFrame.value)) {
+        currentFrame = 0;
+    }
+
+    renderer.render(scene, camera);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await init();
+    animate();
+});
